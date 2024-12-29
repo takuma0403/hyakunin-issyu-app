d8e8998b8180d947c2fe93e8c235a187fa20cb3b
t_takumaru <t.takuma.saga.u@gmail.com>
2024-12-29 23:15:54 +0900

    ふぉーまっと

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script lang="ts">
-export default {};
+export default {}
 </script>
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,14 +1,13 @@
 <template>
-    <GridButtons />
-  </template>
-  
-  <script lang="ts">
-  import GridButtons from '../components/GridButton/GridButton.vue';
-  
-  export default {
-    components: {
-      GridButtons,
-    },
-  };
-  </script>
-  
+  <GridButtons />
+</template>
+
+<script lang="ts">
+import GridButtons from '../components/GridButton/GridButton.vue'
+
+export default {
+  components: {
+    GridButtons,
+  },
+}
+</script>
